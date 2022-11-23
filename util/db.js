23417b3ed72491c516911a620acf4b2eb5b67f7f
Lambertn33 <lambertgithub@gmail.com>
2022-11-23 08:35:38 +0200

    add environment variables

--- util/db.js
+++ util/db.js
@@ -1,7 +1,9 @@
 import Sequelize from 'sequelize';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 //TODO add .env files
-export default new Sequelize('companies', 'postgres', 'lambert12345', {
+export default new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'postgres',
     logging: false
 });
